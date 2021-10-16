c7efa69a1e703fa95456ac1a8e830e9bde7d6ba0
Anton Zhadan <86596115+anton-zhadan@users.noreply.github.com>
2021-10-16 16:08:04 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,11 +7,10 @@ toStart: for(; a<=b; a+=h) {
 
     for(j=1; j<a; j++) {
         console.log (`  ${j}`);
-        factorial *= a;
         if(a%j === 0) {
             continue toStart;
         }
     }
 
-    console.log(`${a}! = ${sumOfFactorials}`);
+    console.log(a = `${sumOfFactorials}`);
 }
